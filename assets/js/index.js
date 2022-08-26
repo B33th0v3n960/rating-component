2103b624595fb28233dae2c08e919badf0ee6ce2
Zhou Sun <zhousun06@gmail.com>
2022-08-26 18:26:39 +0100

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+//Submit Button
+const submitBtn = document.querySelector("#submit");
+const cards = document.querySelectorAll(".rating__card");
+const rate = document.querySelectorAll(".rating__number");
+const tag = document.querySelector(".rating__info");
+let active = document.querySelector(".active--btn");
+
+rate.forEach((number) =>
+  number.addEventListener("click", () => {
+    rate.forEach((element) => element.classList.remove("active--btn"));
+    if (number.classList.contains("active--btn"))
+      number.classList.remove("active--btn");
+    else number.classList.add("active--btn");
+    return (active = document.querySelector(".active--btn"));
+  })
+);
+
+submitBtn.addEventListener("click", () => {
+  if (active) {
+    console.log(cards);
+    cards.forEach((card) => {
+      if (card.classList.contains("hidden")) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+      console.log(active.innerHTML);
+      tag.innerText = `You selected ${active.innerHTML} out of 5`;
+    });
+  } else {
+    cards.forEach((card) => {
+      if (!card.classList.contains("hidden")) {
+        setTimeout(() => card.classList.remove("animate__headShake"), 1000);
+        card.classList.add("animate__headShake");
+      }
+    });
+  }
+});
